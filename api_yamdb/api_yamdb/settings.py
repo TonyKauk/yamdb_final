cf468dbaeb1d4dfabd2b9b05c3c77fb12588512b
Anton Milov <milovanton@gmail.com>
2022-08-10 23:43:44 +0300

    pre-review version

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -7,7 +7,10 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '*',
+    '51.250.20.145'
+]
 
 
 INSTALLED_APPS = [
